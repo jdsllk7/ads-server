373a7f2d814d56cefa6c538cc963784fa94c2850
jdslk <jdslk7@gmail.com>
2019-09-09 20:56:25 +0200

    Still mapping

--- controlPanel.php
+++ controlPanel.php
@@ -224,53 +224,18 @@
 
 
 <script>
+	var map;
 	function initMap() {
-	var map = new google.maps.Map(document.getElementById('map'), {
-		zoom: 4,
-		center: {lat: -13.1339, lng: 27.86}  // Australia.
-	});
-
-	var directionsService = new google.maps.DirectionsService;
-	var directionsRenderer = new google.maps.DirectionsRenderer({
-		draggable: true,
-		map: map,
-		panel: document.getElementById('right-panel')
-	});
-
-	directionsRenderer.addListener('directions_changed', function() {
-		// computeTotalDistance(directionsRenderer.getDirections());
-	});
-
-	displayRoute('Perth, WA', 'Sydney, NSW', directionsService,
-		directionsRenderer);
-	}
-
-	function displayRoute(origin, destination, service, display) {
-	service.route({
-		origin: origin,
-		destination: destination,
-		waypoints: [{location: 'Adelaide, SA'}, {location: 'Broken Hill, NSW'}],
-		travelMode: 'DRIVING',
-		avoidTolls: true
-	}, function(response, status) {
-		if (status === 'OK') {
-		display.setDirections(response);
-		} else {
-		alert('Could not display directions due to: ' + status);
-		}
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: {lat: -13.1339, lng: 27.86},
+		zoom: 4
 	});
 	}
-
-	// function computeTotalDistance(result) {
-	// var total = 0;
-	// var myroute = result.routes[0];
-	// for (var i = 0; i < myroute.legs.length; i++) {
-	// 	total += myroute.legs[i].distance.value;
-	// }
-	// total = total / 1000;
-	// document.getElementById('total').innerHTML = total + ' km';
-	// }
 </script>
+
+	
+
+
 
 
 
